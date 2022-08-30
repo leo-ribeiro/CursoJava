2566a9eb6b7a927e47adfc99df2217905b1c927d
leopempem <leopemribeiro@gmail.com>
2022-08-30 17:26:39 -0300

    Primeiro Commit

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	// Uma sentença de cógo termina com ;
+	public static void main(String[] args) {
+		System.out.println("Primeiro programa Parte #01!");
+		System.out.println("Primeiro programa Parte #02!");
+	}
+}
